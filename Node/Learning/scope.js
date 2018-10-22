9097318ff4c8e6a259cd9157833a39eda2bb4f44
meetparekh09 <meetparekh09@gmail.com>
2018-10-22 16:06:59 -0400

    Nested scope in JavaScript

---
+++ Node/Learning/scope.js
@@ -0,0 +1,13 @@
+function foo(a) {
+    var b = a * 2;
+    var c = 3;
+
+    function bar(str, c) {
+        eval(str);
+        console.log(a, b, c);
+    }
+
+    bar("var b = 2", b * 3);
+}
+
+foo(2);
