826d6a059542759aa5ea06be2b1fa1a4e5d9e9c6
meetparekh09 <meetparekh09@gmail.com>
2018-10-23 12:50:02 -0400

    Doing it the cool way.

--- Node/Learning/closure.js
+++ Node/Learning/closure.js
@@ -30,10 +30,10 @@ baz(); //closure uncurtained
 
 for(var i = 1; i <= 5; i++) {
     // let copy = i; // this works, but let's try IIFE
-    (function() {
-        var j = i; // now this is a copy of i in IIFE scope which will be closed in timer
+    (function(j) {
+        // var j = i; // now this is a copy of i in IIFE scope which will be closed in timer
         setTimeout(function timer() {
             console.log(j); // i won't work as it is still invoked from global scope
         }, j*1000);
-    }());
+    }( i));
 }
