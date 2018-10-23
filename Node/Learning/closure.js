51dec5ffa5b20ac4e2261bb712e2f6d84a2f4703
meetparekh09 <meetparekh09@gmail.com>
2018-10-23 12:49:07 -0400

    playing with closures and scope

--- Node/Learning/closure.js
+++ Node/Learning/closure.js
@@ -1,10 +1,11 @@
+// "use strict";
 // function foo() {
 //     var a = 2;
 //
 //     function bar() {
 //         console.log(a);
 //     }
-// 
+//
 //     bar(); //closure exists but not explicitly observed
 // }
 //
@@ -24,3 +25,15 @@ function foo() {
 var baz = foo();
 
 baz(); //closure uncurtained
+
+
+
+for(var i = 1; i <= 5; i++) {
+    // let copy = i; // this works, but let's try IIFE
+    (function() {
+        var j = i; // now this is a copy of i in IIFE scope which will be closed in timer
+        setTimeout(function timer() {
+            console.log(j); // i won't work as it is still invoked from global scope
+        }, j*1000);
+    }());
+}
