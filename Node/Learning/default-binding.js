4c27b7644c2b57eb6fd3f59ac6616ff25f04aaf6
meetparekh09 <meetparekh09@gmail.com>
2018-10-24 13:02:08 -0400

    default binding of this

---
+++ Node/Learning/default-binding.js
@@ -0,0 +1,13 @@
+global.a = 2;
+// console.log(global.a);
+function foo() {
+    // console.log(this);
+    // console.log(this.foo);
+    // this.a = 3;
+    console.log(this.a);
+}
+
+foo();
+
+
+// console.log(a);
--- Node/Learning/this2.js
+++ Node/Learning/this2.js
@@ -1,14 +1,14 @@
 function foo(num) {
     console.log(num);
 
-    // this.count++; //refers to global object
-    foo.count++;
+    this.count++;
+    // foo.count++;
 }
 
 foo.count = 0;
 
 for(var i = 0; i < 10; i++) {
-    foo(i);
+    foo.call(foo, i);
 }
 
 console.log(foo.count);
