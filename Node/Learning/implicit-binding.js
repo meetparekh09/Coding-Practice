a2c0af5dfb25236ce406c7a955a1e58c6eaaa316
meetparekh09 <meetparekh09@gmail.com>
2018-10-24 13:07:07 -0400

    implicit binding

---
+++ Node/Learning/implicit-binding.js
@@ -0,0 +1,17 @@
+function foo() {
+    console.log(this.a);
+}
+
+var obj2 = {
+    a: 42,
+    foo: foo
+};
+
+var obj1 = {
+    a: 2,
+    foo: foo,
+    obj2: obj2
+};
+
+obj1.foo();
+obj1.obj2.foo();
