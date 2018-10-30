744248d36ff9bd4373afe80ec4aca55b594829ec
meetparekh09 <meetparekh09@gmail.com>
2018-10-29 23:01:28 -0400

    parasitic inheritance

---
+++ Node/Learning/parasitic-inheritance.js
@@ -0,0 +1,29 @@
+function Vehicle() {
+    this.engines = 1;
+}
+
+Vehicle.prototype.ignition = function() {
+    console.log("Turning on engine.");
+}
+
+Vehicle.prototype.drive = function() {
+    console.log("Steering and moving forward!");
+}
+
+function Car() {
+    var car = new Vehicle();
+
+    car.wheels = 4;
+
+    var vehDrive = car.drive;
+
+    car.drive = function() {
+        vehDrive.call(this);
+        console.log("Rolling on all " + this.wheels + " wheels.");
+    }
+
+    return car;
+}
+
+var myCar = new Car();
+myCar.drive();
